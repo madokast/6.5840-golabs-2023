72fc6dbb5512a882211e14a56305c34097bf122b
madokast <578562554@qq.com>
2023-11-09 06:43:13 +0000

    worker logic done

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -3,8 +3,11 @@ package mr
 import (
 	"fmt"
 	"hash/fnv"
+	"io"
 	"log"
 	"net/rpc"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -32,6 +35,8 @@ func Worker(mapf func(string, string) []KeyValue,
 		var mapRes MapTeskRes
 		if call("Coordinator.GetMapTask", &mapReq, &mapRes) {
 			if mapRes.State == 0 || mapRes.State == 1 {
+				kvs := mapf(mapRes.Key, readMapFileContent(mapRes.Key))
+				writeMapResult(kvs, mapRes.TaskId, mapRes.ReduceNum)
 				var doneReq = MapTaskDoneReq{TaskId: mapRes.TaskId}
 				var doneRes MapTaskDoneRes
 				if call("Coordinator.MapTeskDone", &doneReq, &doneRes) {
@@ -40,15 +45,13 @@ func Worker(mapf func(string, string) []KeyValue,
 					}
 				}
 			} else if mapRes.State == 2 {
-
+				time.Sleep(200 * time.Millisecond)
 			} else if mapRes.State == 3 {
 				break
 			} else {
 				panic(mapRes.State)
 			}
 		}
-
-		time.Sleep(200 * time.Millisecond)
 	}
 
 	for { // reducing
@@ -56,6 +59,7 @@ func Worker(mapf func(string, string) []KeyValue,
 		var rdcTaskRes ReduceTaskRes
 		if call("Coordinator.GetReduceTask", &rdcTaskReq, &rdcTaskRes) {
 			if rdcTaskRes.State == 0 || rdcTaskRes.State == 1 {
+				makeReduceResult(rdcTaskRes.MapNumber, rdcTaskRes.TaskId, reducef)
 				var doneReq = ReduceTaskDoneReq{TaskId: rdcTaskRes.TaskId}
 				var doneRes ReduceTaskDoneRes
 				if call("Coordinator.ReduceTaskDone", &doneReq, &doneRes) {
@@ -64,7 +68,7 @@ func Worker(mapf func(string, string) []KeyValue,
 					}
 				}
 			} else if rdcTaskRes.State == 2 {
-
+				time.Sleep(200 * time.Millisecond)
 			} else if rdcTaskRes.State == 3 {
 				break
 			} else {
@@ -76,6 +80,85 @@ func Worker(mapf func(string, string) []KeyValue,
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+}
+
+func readMapFileContent(filename string) string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	return string(content)
+}
+
+func writeMapResult(kvs []KeyValue, mapTaskId int, reduceNumber int) {
+	var files = make([]*os.File, reduceNumber)
+	var err error
+	for rn := 0; rn < reduceNumber; rn++ {
+		files[rn], err = os.Create(intermediateFileName(mapTaskId, rn))
+		if err != nil {
+			log.Fatalf("cannot open %v %v", intermediateFileName(mapTaskId, rn), err)
+		}
+	}
+	for _, kv := range kvs {
+		var rn = ihash(kv.Key) % reduceNumber
+		_, err = files[rn].WriteString(fmt.Sprintf("%s\a%s\n", kv.Key, kv.Value))
+		if err != nil {
+			log.Fatalf("cannot write %v %v", intermediateFileName(mapTaskId, rn), err)
+		}
+	}
+	for rn, file := range files {
+		err = file.Close()
+		if err != nil {
+			log.Fatalf("cannot close %v %v", intermediateFileName(mapTaskId, rn), err)
+		}
+	}
+	log.Printf("map[%d, %d] done\n", mapTaskId, reduceNumber)
+}
+
+func makeReduceResult(mapNumber, reduceTaskId int, reducef func(string, []string) string) {
+	reduceFileName := reducedFileName(reduceTaskId)
+	reduceFile, err := os.Create(reduceFileName)
+	if err != nil {
+		log.Fatalf("cannot create %v %v", reduceFileName, err)
+	}
+	for mapTaskId := 0; mapTaskId < mapNumber; mapTaskId++ {
+		interFile := intermediateFileName(mapTaskId, reduceTaskId)
+		content := readMapFileContent(interFile)
+		err := os.Remove(interFile)
+		if err != nil {
+			log.Fatalf("cannot delete %v %v", interFile, err)
+		}
+		var m = map[string][]string{}
+		for _, line := range strings.Split(content, "\n") {
+			if line == "" {
+				continue
+			}
+			kv := strings.Split(line, "\a")
+			m[kv[0]] = append(m[kv[0]], kv[1])
+		}
+		for k, v := range m {
+			r := reducef(k, v)
+			reduceFile.WriteString(fmt.Sprintf("%v %v\n", k, r))
+		}
+
+	}
+	err = reduceFile.Close()
+	if err != nil {
+		log.Fatalf("cannot close %v %v", reduceFileName, err)
+	}
+}
+
+func intermediateFileName(mapTaskId, reduceTaskId int) string {
+	return fmt.Sprintf("mr-%d-%d", mapTaskId, reduceTaskId)
+}
+
+func reducedFileName(reduceTaskId int) string {
+	return fmt.Sprintf("mr-out-%d", reduceTaskId)
 }
 
 // example function to show how to make an RPC call to the coordinator.
